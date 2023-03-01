f5950fd47a9a0cad0deb5d592cfa01c6926eae9b
Deni <00amfibinator00@mail.ru>
2023-03-01 23:09:26 +0300

    made changes to the navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,14 +4,16 @@ const Navigation = () => {
   return (
     <div className='navigation'>
       <div className="navigation__inner">
-        <li className="navigation__inner-text active">Холодные закуски</li>
-        <li className="navigation__inner-text">Горячие закуски</li>
-        <li className="navigation__inner-text">Мясные блюда</li>
-        <li className="navigation__inner-text">Супы</li>
-        <li className="navigation__inner-text">Рыбные блюда</li>
-        <li className="navigation__inner-text">Гриль меню</li>
-        <li className="navigation__inner-text">Фирменные блюда</li>
-        <li className="navigation__inner-text">Напитки</li>
+        <ul className="navigation__inner-list">
+          <li className="navigation__inner-list-text active">Холодные закуски</li>
+          <li className="navigation__inner-list-text">Горячие закуски</li>
+          <li className="navigation__inner-list-text">Мясные блюда</li>
+          <li className="navigation__inner-list-text">Супы</li>
+          <li className="navigation__inner-list-text">Рыбные блюда</li>
+          <li className="navigation__inner-list-text">Гриль меню</li>
+          <li className="navigation__inner-list-text">Фирменные блюда</li>
+          <li className="navigation__inner-list-text">Напитки</li>
+        </ul>
       </div>
     </div>
   )
