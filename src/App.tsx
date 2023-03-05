1996fbad3056ffc439c1c8eedd327218fa1f4a23
Deni <00amfibinator00@mail.ru>
2023-03-06 00:44:48 +0300

    almost made info

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Header from './components/Header'
 import Navigation from './components/Navigation'
+import Info from './components/Info'
 import './scss/app.scss'; 
 
 function App() {
   return <div className="App">
     <Header />
     <Navigation />
+    <Info />
   </div>
 }
 
