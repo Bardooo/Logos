3098f3b8fcc597e2085182bdb48249622e3a3f1c
Deni <00amfibinator00@mail.ru>
2023-03-15 01:32:09 +0300

    made up changes in menu-2

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import MenuSwiper from './MenuSwiper'
 
+type navigationTitlesType = {
+  id: number,
+  title: string
+}
+type itemsType = {
+  id: number,
+  imageUrl: string,
+  title: string,
+  weight: number,
+  text: string,
+  price: number
+}
+
 type MenuProps = {
-  items: {},
-  navigationTitles: any[]
+  items: itemsType[],
+  navigationTitles: navigationTitlesType[]
 }
 
 const Menu: React.FC<MenuProps> = ({items, navigationTitles}) => {
   return (
     <div className='menu'>
-      {navigationTitles.map((item: {id: Number, title: String}) => (
+      {navigationTitles.map((item) => (
         <div className="menu__inner" >
           <div className="menu__inner-block">
             <h1 className="menu__inner-block-title">{item.title}</h1>
