398495734c1cabcaf0679c929397cb0cac52e5ca
Deni <00amfibinator00@mail.ru>
2023-03-16 01:56:19 +0300

    made up the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,30 @@
 import React from 'react'
+import arrow from '../assets/img/arrow.svg'
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div className="footer__inner">
         <div className="footer__inner-up">
-          <div className="footer__inner-up-button"></div>
+          <div className="footer__inner-up-button">
+            <img src={arrow} alt="arrow" className="footer__inner-up-button-img" />
+          </div>
         </div>
         <div className="footer__inner-info">
           <h4 className="footer__inner-info-title">LOGOS</h4>
-          <p className="footer__inner-info-text">© ООО СК «АПШЕРОН»</p>
-          <p className="footer__inner-info-span">Все права защищены. 2010-2020</p>
+          <p className="footer__inner-info-text">© ООО СК «АПШЕРОН» <br/> Все права защищены. 2010-2020</p>
           <ul className="footer__inner-info-links">
-            <li className="footer__inner-links-item">Пользовательское соглашение</li>
-            <li className="footer__inner-links-item">Карта сайта</li>
-            <li className="footer__inner-links-item">Политика конфиденциальности</li>
+            <li className="footer__inner-info-links-item">Пользовательское соглашение</li>
+            <li className="footer__inner-info-links-item">Карта сайта</li>
+            <li className="footer__inner-info-links-item">Политика конфиденциальности</li>
           </ul>
         </div>
-        <div className="footer__inner-nav">
-          <ul className="footer__inner-nav-list">
-            <li className="footer__inner-nav-list-item">О ресторане</li>
-            <li className="footer__inner-nav-list-item">Условия доставки</li>
-            <li className="footer__inner-nav-list-item">Возврат товара</li>
-            <li className="footer__inner-nav-list-item">Акции</li>
-          </ul>
-        </div>
+        <ul className="footer__inner-nav">
+          <li className="footer__inner-nav-item">О ресторане</li>
+          <li className="footer__inner-nav-item">Условия доставки</li>
+          <li className="footer__inner-nav-item">Возврат товара</li>
+          <li className="footer__inner-nav-item">Акции</li>
+        </ul>
       </div>
     </div>
   )
