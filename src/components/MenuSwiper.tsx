fe5516549396996f94e6042a1c27234d5ac5cd8e
Deni <00amfibinator00@mail.ru>
2023-03-26 17:30:05 +0300

    adapted menu

--- src/components/MenuSwiper.tsx
+++ src/components/MenuSwiper.tsx
@@ -5,15 +5,49 @@ import {itemsType} from './Menu'
 
 import 'swiper/css';
 
+type breakpointsType = {
+  320: {
+    slidesPerView: number;
+    spaceBetween: number;
+  };
+  450: {
+    slidesPerView: number;
+    spaceBetween: number;
+  };
+  552: {
+    slidesPerView: number;
+    spaceBetween: number;
+  };
+  700: {
+    slidesPerView: number;
+    spaceBetween: number;
+  };
+  784: {
+    slidesPerView: number;
+    spaceBetween: number;
+  };
+  1090: {
+    slidesPerView: number;
+    spaceBetween: number;
+  };
+  1470: {
+    slidesPerView: number;
+    spaceBetween: number;
+  };
+};
+
 type MenuSwiperProps = {
   items: itemsType[],
+  breakpoints: breakpointsType
 }
 
-const MenuSwiper: React.FC<MenuSwiperProps> = ({items}) => {  
+const MenuSwiper: React.FC<MenuSwiperProps> = ({items, breakpoints}) => {  
   return (
     <Swiper
-      spaceBetween={-140}
-      slidesPerView={4}
+    loop={true}
+      spaceBetween={10}
+      slidesPerView={1}
+      breakpoints={breakpoints}
     >
       {items.map((item: any, index: Number) => (
         <SwiperSlide><Card key={index} {...item} /></SwiperSlide>
