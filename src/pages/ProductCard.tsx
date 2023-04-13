207d7b8947f6a793260c559d3f2431835a4982bc
Deni <00amfibinator00@mail.ru>
2023-04-13 04:51:13 +0300

    first step

--- src/pages/ProductCard.tsx
+++ src/pages/ProductCard.tsx
@@ -2,6 +2,7 @@ import MenuSwiper from '../components/MenuSwiper'
 import { itemsType } from '../components/Menu'
 
 import backImg from '../assets/img/back-img.svg'
+import busket from '../assets/img/busket.svg'
 
 type ProductCardProps = {
   items: itemsType[],
@@ -19,9 +20,40 @@ const ProductCard: React.FC<ProductCardProps> = ({ items }) => {
           <div className="product-card__inner-main-item">
             <img className='product-card__inner-main-item-img' src={items[0].imageUrl} alt="product-img" />
             <div className="product-card__inner-main-item-info">
-              <h5 className="product-card__inner-main-item-info-title">{items[0].title}</h5>
-              <p className="product-card__inner-main-item-info-text">помидор, сыр фета, масло подсолнечное, капуста пекинская, перец сладкий красный, огурцы, оливки без косточек</p>
-              
+              <div className="product-card__inner-main-item-info-top">
+                <div className="product-card__inner-main-item-info-top-text">
+                  <h5 className="product-card__inner-main-item-info-top-text-title">{items[0].title}</h5>
+                  <p className="product-card__inner-main-item-info-top-text-main-text">помидор, сыр фета, масло подсолнечное, капуста пекинская, перец сладкий красный, огурцы, оливки без косточек</p>
+                </div>
+                <div className="product-card__inner-main-item-info-top-main">
+                  <p className="product-card__inner-main-item-info-top-main-weight">Вес: 210 г</p>
+                  <div className="product-card__inner-main-item-info-top-main-cost">
+                    <div className="product-card__inner-main-item-info-top-main-cost-btn">
+                      <p className="product-card__inner-main-item-info-top-main-cost-btn-text">Корзина</p>
+                      <img className="product-card__inner-main-item-info-top-main-cost-btn-img" src={busket} alt="busket" />
+                    </div>
+                    <p className="product-card__inner-main-item-info-top-main-cost-text">259 ₽</p>
+                  </div>
+                </div>
+              </div>
+              <div className="product-card__inner-main-item-info-buttom">
+                <div className="product-card__inner-main-item-info-buttom-wrap">
+                  <div className="product-card__inner-main-item-info-buttom-wrap-text">
+                    <p className="product-card__inner-main-item-info-buttom-wrap-text-item">Белки</p>
+                    <p className="product-card__inner-main-item-info-buttom-wrap-text-item">Жиры</p>
+                    <p className="product-card__inner-main-item-info-buttom-wrap-text-item">Углеводы</p>
+                    <p className="product-card__inner-main-item-info-buttom-wrap-text-item">Ккал</p>
+                    <p className="product-card__inner-main-item-info-buttom-wrap-text-item">Вес</p>
+                  </div>
+                </div>
+                <div className="product-card__inner-main-item-info-buttom-numbers">
+                  <p className="product-card__inner-main-item-info-buttom-numbers-item">17.23</p>
+                  <p className="product-card__inner-main-item-info-buttom-numbers-item">7.63</p>
+                  <p className="product-card__inner-main-item-info-buttom-numbers-item">22.35</p>
+                  <p className="product-card__inner-main-item-info-buttom-numbers-item">234</p>
+                  <p className="product-card__inner-main-item-info-buttom-numbers-item">210 г</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
