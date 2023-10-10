647c27801452f7f343a201a5949c795d7d71d564
Deni <00amfibinator00@mail.ru>
2023-10-11 00:44:11 +0300

    внедрил redux

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -4,47 +4,47 @@ import busket from '../assets/img/busket.svg'
 
 const ProductCard = ({ item }) => {
   return (
-    <div className="product-card__inner-main-item">
+    <div className="product-card">
       <img
-        className="product-card__inner-main-item-img"
+        className="product-card__img"
         src={item.imageUrl}
         alt="product-img"
       />
-      <div className="product-card__inner-main-item-info">
-        <div className="product-card__inner-main-item-info-top">
-          <div className="product-card__inner-main-item-info-top-text">
-            <h5 className="product-card__inner-main-item-info-top-text-title">{item.title}</h5>
-            <p className="product-card__inner-main-item-info-top-text-main-text">
+      <div className="product-card__info">
+        <div className="product-card__top">
+          <div className="product-card__text">
+            <h5 className="product-card__title">{item.title}</h5>
+            <p className="product-card__main-text">
               {item.text}
             </p>
           </div>
-          <div className="product-card__inner-main-item-info-top-main">
-            <p className="product-card__inner-main-item-info-top-main-weight">
+          <div className="product-card__main">
+            <p className="product-card__weight">
               Вес: {item.weight} г
             </p>
-            <div className="product-card__inner-main-item-info-top-main-cost">
-              <div className="product-card__inner-main-item-info-top-main-cost-btn">
-                <p className="product-card__inner-main-item-info-top-main-cost-btn-text">Корзина</p>
+            <div className="product-card__cost">
+              <div className="product-card__btn">
+                <p className="product-card__btn-text">Корзина</p>
                 <img
-                  className="product-card__inner-main-item-info-top-main-cost-btn-img"
+                  className="product-card__btn-img"
                   src={busket}
                   alt="busket"
                 />
               </div>
-              <p className="product-card__inner-main-item-info-top-main-cost-text">
+              <p className="product-card__cost-text">
                 {item.price} ₽
               </p>
             </div>
           </div>
         </div>
-        <div className="product-card__inner-main-item-info-buttom">
-          <div className="product-card__inner-main-item-info-buttom-wrap">
+        <div className="product-card__buttom">
+          <div className="product-card__wrap">
             {item.info.map((item, index) => (
-              <div className="product-card__inner-main-item-info-buttom-wrap-item" key={index}>
-                <p className="product-card__inner-main-item-info-buttom-wrap-item-text">
+              <div className="product-card__item" key={index}>
+                <p className="product-card__item-text">
                   {item.title}
                 </p>
-                <p className="product-card__inner-main-item-info-buttom-wrap-item-number">
+                <p className="product-card__item-number">
                   {item.value}
                 </p>
               </div>
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,48 +1,58 @@
 import { Link, useParams } from 'react-router-dom'
 import React from 'react'
-import axios from 'axios'
 import MenuSwiper from '../components/MenuSwiper'
 
 import backImg from '../assets/img/back-img.svg'
 import ProductCard from '../components/ProductCard'
-import { itemsType } from '../components/Menu'
+import { fetchFood } from '../redux/food/asyncActions'
+import { useAppDispatch } from '../redux/store'
+import { useSelector } from 'react-redux'
+import { selectFoodData } from '../redux/food/selectors'
 
 const Product = () => {
-  const [items, setItems] = React.useState<itemsType[]>()
   const { id } = useParams()
-  
+  const dispatch = useAppDispatch()
+
+  const { items, status } = useSelector(selectFoodData);
+
+  const getFood = async () => {
+    dispatch(fetchFood())
+  }
+
   React.useEffect(() => {
-    async function fetchFood() {
-      try {
-        const {data} = await axios.get('https://6403abed3bdc59fa8f2acb30.mockapi.io/items')        
-        setItems(data)
-      } catch (error) {
-        alert('Ошибка при получении данных')
-      }
-    }
-    fetchFood()
-  }, [])  
+    window.scrollTo(0, 0);
+    getFood();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   
   return (
-    <section className='product-card'>
-      <div className="product-card__inner">
-        <div className="product-card__inner-main">
+    <section className='product'>
+      <div className="product__inner">
+        <div className="product__main">
           <Link to='/'>
-            <div className="product-card__inner-main-back">
-              <img className="product-card__inner-main-back-img" src={backImg} alt="back-img"/>
-              <p className="product-card__inner-main-back-text">Вернуться назад</p>
+            <div className="product__back">
+              <img className="product__img" src={backImg} alt="back-img"/>
+              <p className="product__text">Вернуться назад</p>
             </div>
           </Link>
-          {items === undefined ? (
-            <>Получаем данные...</>
+          {status === 'error' ? (
+            <div className="content__error-info">
+              <h2>Произошла ошибка</h2>
+              <p>Не удалось получить пиццы. Попробуйте повторить попытку позже</p>
+            </div>
           ) : (
-            <ProductCard item={items[id]}/>
+            <>{status === 'loading' ? <>Получаем данные</> : <ProductCard item={items[id]} />}</>
           )}
         </div>
-        <div className="product-card__inner-extra">
-          <h4 className="product-card__inner-extra-title title">С ЭТИМ ТОВАРОМ ПОКУПАЮТ</h4>
-          {items === undefined ? (<>Получаем данные...</>) : (
-            <MenuSwiper items={items} />
+        <div className="product__extra">
+          <h4 className="product__title">С ЭТИМ ТОВАРОМ ПОКУПАЮТ</h4>
+          {status === 'error' ? (
+            <div className="content__error-info">
+              <h2>Произошла ошибка</h2>
+              <p>Не удалось получить пиццы. Попробуйте повторить попытку позже</p>
+            </div>
+          ) : (
+            <>{status === 'loading' ? <>Получаем данные</> : <MenuSwiper items={items} />}</>
           )}
         </div>
       </div>
