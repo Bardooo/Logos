238fa8d6e67c09c219eab279fc43a5a278b21e2f
Deni <00amfibinator00@mail.ru>
2023-10-30 01:43:03 +0300

    внес правки

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,25 +1,31 @@
-import cartItemImg from '../assets/img/cart-item.jpg'
-import plusImg from '../assets/img/plus.svg'
-import minusImg from '../assets/img/minus.svg'
-import deleteImg from '../assets/img/delete.svg'
+import cartItemImg from '../assets/img/cart-item.jpg';
+import plusImg from '../assets/img/plus.svg';
+import minusImg from '../assets/img/minus.svg';
+import deleteImg from '../assets/img/delete.svg';
 
 const CartItem = () => {
   return (
     <div className="cart-item">
       <img className="cart-item__img" src={cartItemImg} alt="cart-item-img" />
-      <div className="cart-item__text">
-        <h4 className="cart-item__text-title">ПИЦЦА ДВОЙНАЯ ПЕППЕРОНИ</h4>
-        <p className="cart-item__text-value">
-          Кальмары, мидии, креветки, сыр маасдам, красный лук, микс оливок, базилик, соус песто
-        </p>
+      <div className="cart-item__main">
+        <div className="cart-item__text">
+          <h4 className="cart-item__text-title">ПИЦЦА ДВОЙНАЯ ПЕППЕРОНИ</h4>
+          <p className="cart-item__text-value">
+            Кальмары, мидии, креветки, сыр маасдам, красный лук, микс оливок, базилик, соус песто
+          </p>
+        </div>
+        <div className="cart-item__right">
+          <div className="cart-item__quantity">
+            <img className="cart-item__quantity-img" src={minusImg} alt="cart-arrow-left" />
+            <p className="cart-item__quantity-counter">1</p>
+            <img className="cart-item__quantity-img" src={plusImg} alt="cart-arrow-right" />
+          </div>
+          <div className="cart-item__wrap">
+            <p className="cart-item__price">1640 ₽</p>
+            <img className="cart-item__delete" src={deleteImg} alt="button-delete" />
+          </div>
+        </div>
       </div>
-      <div className="cart-item__quantity">
-        <img className="cart-item__quantity-img" src={minusImg} alt="cart-arrow-left" />
-        <p className="cart-item__quantity-counter">1</p>
-        <img className="cart-item__quantity-img" src={plusImg} alt="cart-arrow-right" />
-      </div>
-      <p className="cart-item__price">1640 ₽</p>
-      <img className="cart-item__delete" src={deleteImg} alt="button-delete" />
     </div>
   );
 };
